63dbb5d39fd69f840c2912f6d72322ef6186c93e
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-09 20:59:06 -0500

    start

---
+++ index.js
@@ -0,0 +1,27 @@
+var myNumber = 0;
+var myBoolean = true;
+var myString = "This is a string";
+
+//MATH
+var sum = 1 + 2;
+var multiply = 8 * 8;
+var sub = 10 - 5;
+var div = 100 / 50;
+var product = 1 + 2 / 3;
+console.log(product);
+
+
+//BOOLEANS
+var trueBoolean = true;
+var falseBoolean = false;
+
+
+//string
+var string = "Hello, World";
+var capString = string.toUpperCase();
+console.log(capString);
+
+
+var abc = "Visit Microsoft!";
+var xyz = abc.replace("Microsoft!", "W3School");
+console.log(xyz);
